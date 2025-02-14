c751abd041c37f651170e9e30313cb89f250daf1
ashiyaman <ashiya1005@gmail.com>
2025-02-14 12:05:42 +0530

    feat: Integrate delete book function with backend

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,13 +2,29 @@ import useFetch from "../useFetch"
 
 const Books = () => {
     const { data, loading, error } = useFetch('https://libra-core-sigma.vercel.app/books')
+
+    const deleteHandler = async (bookId) => {
+        try{
+            const response = await fetch(`https://libra-core-sigma.vercel.app/books/${bookId}`, 
+                {method: 'DELETE'}
+            )
+            if(!response.ok){
+                throw 'Unable to delete Book.'
+            }
+            const data = await response.json()
+            window.location.reload()
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
    
     return (
         <div>
             <h2>All Books</h2>
             <ul>
                 {data?.map(book => (
-                    <li key={book._id} >{book.title}</li>
+                    <li key={book._id} >{book.title} <button onClick={() => deleteHandler(book._id)}>Delete</button></li>
                 ))}
             </ul>
         </div>
